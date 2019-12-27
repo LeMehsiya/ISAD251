b61683110da5267a48dd7342c091f0068ff0cb54
LeMehsiya <35083273+LeMehsiya@users.noreply.github.com>
2019-12-27 00:46:42 +0000

    Home files
    Files which display main base of the project

---
+++ functions.php
@@ -0,0 +1,70 @@
+<?php
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'shoppingcart';
+    try {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	die ('Failed to connect to database!');
+    }
+}
+function template_header($title) {
+// Get the amount of items in the shopping cart
+$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,minimum-scale=1">
+        <title>$title</title>
+        
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body>
+        <header>
+            <div class="content-wrapper">
+                <h1>ChinaTown Tea</h1>
+                <nav>
+                    <a href="index.php">Home</a>
+                    <a href="index.php?page=products">Products</a>
+                    <a href="index.php?page=myaccount">My Account</a>
+                    <a href="index.php?page=adminindex">My Admin</a>
+                </nav>
+                <div class="link-icons">
+                    <div class="search">
+						<i class="fas fa-search"></i>
+						<input type="text" placeholder="Search...">
+					</div>
+                    <a href="index.php?page=cart">
+						<i class="fas fa-shopping-cart"></i>
+						<span>$num_items_in_cart</span>
+					</a>
+					<a class="responsive-toggle" href="#">
+						<i class="fas fa-bars"></i>
+					</a>
+                </div>
+            </div>
+        </header>
+        <main>
+EOT;
+}
+// Template footer
+function template_footer() {
+$year = date('Y');
+echo <<<EOT
+        </main>
+        <footer>
+            <div class="content-wrapper">
+                <p>&copy; $year, ChinaTown Tea</p>
+            </div>
+        </footer>
+        <script src="script.js"></script>
+    </body>
+</html>
+EOT;
+}
+?>
---
+++ home.php
@@ -0,0 +1,41 @@
+<?php
+
+$stmt = $pdo->prepare('SELECT * FROM products ORDER BY date_added DESC LIMIT 4');
+$stmt->execute();
+$recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?=template_header('Home')?>
+
+<style>
+img {
+  width: 100%;
+}
+</style>
+
+<div class="HomeBanner">
+    <img src="imgs/chinatown3.jpg" alt="Chinatown" width="128" height="384">
+    <h2>ChinaTown Tea</h2>
+    <p>Rich Old Tea</p>
+</div>
+
+</div>
+<div class="recentlyadded content-wrapper">
+    <h2>Recently Added Products</h2>
+    <div class="products">
+        <?php foreach ($recently_added_products as $product): ?>
+        <a href="index.php?page=product&id=<?=$product['id']?>" class="product">
+            <img src="imgs/<?=$product['img']?>" width="200" height="200" alt="<?=$product['name']?>">
+            <span class="name"><?=$product['name']?></span>
+            <span class="price">
+                &pound;<?=$product['price']?>
+                <?php if ($product['rrp'] > 0): ?>
+                <span class="rrp">&pound;<?=$product['rrp']?></span>
+                <?php endif; ?>
+            </span>
+        </a>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+<?=template_footer()?>
